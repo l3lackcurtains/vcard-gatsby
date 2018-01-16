dc526779915131935d4632a6c949e330867ca470
Madhav Poudel <l3lackcurtains@gmail.com>
2018-01-16 09:45:50 +0545

    Added experiiences and educations

---
+++ src/components/Educations.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component'
+import styled from 'styled-components'
+import { Grid, Row, Col } from 'react-flexbox-grid'
+import FaBook from 'react-icons/lib/fa/book'
+import 'react-vertical-timeline-component/style.min.css'
+
+
+const EducationsWrapper = styled.div`
+    .vertical-timeline{
+        width: 100%;
+    }
+    @media only screen and (min-width: 1170px) {
+        .vertical-timeline-element {
+            margin: 1em 0;
+        }
+        .vertical-timeline-element-content .vertical-timeline-element-date {
+            position: absolute;
+            width: 100%;
+            left: 145%;
+            top: 6px;
+            font-size: 16px;
+            font-size: 1rem;
+        }
+        .vertical-timeline-element:nth-child(even):not(.vertical-timeline-element--left) .vertical-timeline-element-content .vertical-timeline-element-date {
+            left: auto;
+            right: 145%;
+            text-align: right;
+        }
+        .vertical-timeline-element-content::before {
+            border-left-color: #e3e3e3;
+        }
+        .vertical-timeline-element.vertical-timeline-element--right .vertical-timeline-element-content::before, .vertical-timeline-element:nth-child(even):not(.vertical-timeline-element--left) .vertical-timeline-element-content::before {
+            border-right-color: #e3e3e3;
+        }
+    }
+    /* vertical line */
+    .vertical-timeline::before {
+        background: #e3e3e3;
+    }
+    .vertical-timeline-element-content {
+        box-shadow: 0px 1px 5px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12);
+    }
+    .vertical-timeline-element-title {
+        margin-bottom: 16px;
+    }
+    @media only screen and (max-width: 1170px) {
+        .vertical-timeline-element-content::before {
+            border-right-color: #e3e3e3;
+        }
+    }
+`
+class Educations extends Component {
+    render() {
+        return (
+            <Grid>
+                <Row>
+                    <Col xs={12} md={10} mdOffset={1}>
+                        <EducationsWrapper>
+                            <h1>Education History</h1>
+                            <VerticalTimeline>
+                                <VerticalTimelineElement
+                                    className="vertical-timeline-element--work"
+                                    date="2011 - present"
+                                    iconStyle={{ background: 'rgb(12, 120, 143)', color: '#fff' }}
+                                    icon={<FaBook />}
+                                >
+                                    <h3 className="vertical-timeline-element-title">Creative Director</h3>
+                                    <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                                    <p>
+                                    Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                                    </p>
+                                </VerticalTimelineElement>
+                                <VerticalTimelineElement
+                                    className="vertical-timeline-element--work"
+                                    date="2010 - 2011"
+                                    iconStyle={{ background: 'rgb(12, 120, 143)', color: '#fff' }}
+                                    icon={<FaBook />}
+                                >
+                                    <h3 className="vertical-timeline-element-title">Art Director</h3>
+                                    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                                    <p>
+                                    Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                                    </p>
+                                </VerticalTimelineElement>
+                                <VerticalTimelineElement
+                                    className="vertical-timeline-element--work"
+                                    date="2008 - 2010"
+                                    iconStyle={{ background: 'rgb(12, 120, 143)', color: '#fff' }}
+                                    icon={<FaBook />}
+                                >
+                                    <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                                    <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+                                    <p>
+                                    User Experience, Visual Design
+                                    </p>
+                                </VerticalTimelineElement>
+                                <VerticalTimelineElement
+                                    className="vertical-timeline-element--work"
+                                    date="2006 - 2008"
+                                    iconStyle={{ background: 'rgb(12, 120, 143)', color: '#fff' }}
+                                    icon={<FaBook />}
+                                >
+                                    <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                                    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                                    <p>
+                                    User Experience, Visual Design
+                                    </p>
+                                </VerticalTimelineElement>
+                            </VerticalTimeline>
+                        </EducationsWrapper>
+                    </Col>
+                </Row>
+            </Grid>
+        )
+    }
+}
+
+export default Educations
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'material-ui/Button';
-import AddIcon from 'material-ui-icons/Add';
-import FaGithub from 'react-icons/lib/fa/github'
-import FaTwitter from 'react-icons/lib/fa/twitter'
-import FaFacebook from 'react-icons/lib/fa/facebook'
-import FaMedium from 'react-icons/lib/fa/medium'
-import FaInstagram from 'react-icons/lib/fa/instagram'
-import Particles from 'react-particles-js'
-import classNames from 'classnames';
-import Typist from 'react-typist';
-import Dialog, {
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
-} from 'material-ui/Dialog';
-import Typography from 'material-ui/Typography';
 import { withStyles } from 'material-ui/styles';
-import { Grid, Row, Col } from 'react-flexbox-grid';
-import Avatar from 'material-ui/Avatar';
-
 import Header from '../components/Header'
 import Intro from '../components/Intro'
 import Skills from '../components/Skills'
 import Portfolio from '../components/Portfolio'
+import Experiences from '../components/Experiences'
+import Educations from '../components/Educations'
 
 import withRoot from '../withRoot';
-import particlesParam from '../utils/particles'
 
 const styles = theme => ({
   root: {
-    textAlign: 'center',
+    //
   },
 });
 
@@ -46,6 +27,8 @@ class Index extends React.Component {
         <Intro />
         <Skills />
         <Portfolio />
+        <Experiences />
+        <Educations />
       </div>
     );
   }
