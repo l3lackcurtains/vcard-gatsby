67f143ecf3bf789da4b4840678e3670389829135
Madhav Poudel <king.ov.universe@gmail.com>
2018-07-26 23:12:36 +0545

    Some Changes on readme

--- README.md
+++ README.md
@@ -1,21 +1,11 @@
-# Gatsby example
+# VCard Gatsby Personal Website
 
-## How to use
+Being implemented on my [Personal VCard](http://madhavpoudel.com.np) website
 
-Download the example [or clone the repo](https://github.com/mui-org/material-ui):
+- To Install and run follow the commands:
 
-```bash
-curl https://codeload.github.com/mui-org/material-ui/tar.gz/v1-beta | tar -xz --strip=2  material-ui-1-beta/examples/gatsby
-cd gatsby
-```
-
-Install it and run:
-
-```bash
-npm install
-npm run develop
-```
-
-## The idea behind the example
-
-[Gatsby](https://github.com/gatsbyjs/gatsby) is a static site generator for React.
+  ```bash
+  yarn
+  yarn build
+  yarn start
+  ```
--- src/withRoot.js
+++ src/withRoot.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { MuiThemeProvider } from 'material-ui/styles';
-import Reboot from 'material-ui/Reboot';
-import getPageContext from './getPageContext';
+import React from "react";
+import PropTypes from "prop-types";
+import { MuiThemeProvider } from "material-ui/styles";
+import Reboot from "material-ui/Reboot";
+import getPageContext from "./getPageContext";
 
 function withRoot(Component) {
   class WithRoot extends React.Component {
@@ -12,7 +12,7 @@ function withRoot(Component) {
 
     componentDidMount() {
       // Remove the server-side injected CSS.
-      const jssStyles = document.querySelector('#jss-server-side');
+      const jssStyles = document.querySelector("#jss-server-side");
       if (jssStyles && jssStyles.parentNode) {
         jssStyles.parentNode.removeChild(jssStyles);
       }
@@ -36,7 +36,7 @@ function withRoot(Component) {
   }
 
   WithRoot.propTypes = {
-    pageContext: PropTypes.object,
+    pageContext: PropTypes.object
   };
 
   return WithRoot;
